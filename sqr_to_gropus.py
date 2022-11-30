a175eb5f8f3051701dbd2be8db2cfc7c7f8411a4
pabloortegaa <63413550+pabloortegaa@users.noreply.github.com>
2022-11-30 02:20:54 +0100

    find claimeven

--- sqr_to_gropus.py
+++ sqr_to_gropus.py
@@ -63,9 +63,49 @@ def square_to_groups(board,player):
     return square_to_group
 
 
+square_to_groups=square_to_groups(initial_board,"X")
+
+# Convert each application of each rule into a Solution.
+solutions=set()
+group_to_solution={}
        
-square_to_groups(initial_board,"X")
-print("-----------------")
-#square_to_groups(other_board,"X")
+#Claimeven
+def find_claimevens(board):
+    """Finds all the claimable evens on a board. Can be used to determine a winner or draw.
+
+    Required: 2 squares directly above each other. Both squares must be empty. The upper square must be even.
+    
+    Returns:
+        List with all the claimevens.
+        Claimeven represented with (row, col) of the lower (odd) square.
+    """
+    claimevens = []
+    for row in range(0, len(board), 2): # Steps of 2 to reach only even rows
+        for col in range(len(board[0])):
+            if board[row][col] == '.':
+                claimevens.append((row, col))
+
+    return claimevens
+claimevens=find_claimevens(initial_board)
+
+def from_claimeven(claimeven,square_to_groups):
+    rule="claimeven"
+    groups= claimeven[0] # claimeven.upper
+    if groups:
+        return{"squares":claimeven,"groups":groups,"rule":rule}
+    
+for claimeven in claimevens:
+    solution=from_claimeven(claimeven,square_to_groups)
+    if solution:
+        solutions.add(solution)
+        if solution["groups"] not in group_to_solution:
+            group_to_solution[solution["groups"]]=set()
+        group_to_solution[solution["groups"]].add(solution)
+    
+        
+
+
+
+
 
             
